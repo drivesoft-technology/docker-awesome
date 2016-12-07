2a9748e03053da482cd7bc3ed5097b91c4118830
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2016-12-08 00:51:40 +0700

    Create install-docker-engine-on-ubuntu16.sh

---
+++ docker-install/install-docker-engine-on-ubuntu16.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get install apt-transport-https ca-certificates -y
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+
+sudo apt-get install -y docker-engine
+
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
+
+sudo service docker start
+sudo docker info
