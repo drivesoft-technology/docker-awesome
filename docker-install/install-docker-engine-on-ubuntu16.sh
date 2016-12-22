c7407e703d3d4f21f02d6dcd03fda086fb674c7a
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2016-12-22 19:40:42 +0700

    Update install-docker-engine-on-ubuntu16.sh

--- docker-install/install-docker-engine-on-ubuntu16.sh
+++ docker-install/install-docker-engine-on-ubuntu16.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 sudo apt-get install apt-transport-https ca-certificates -y
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
