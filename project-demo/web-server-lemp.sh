8e393599e3acb8dfa516ffbb9eff30fafb2118f0
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2016-12-22 19:51:36 +0700

    Create web-server-lemp.sh

---
+++ project-demo/web-server-lemp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo mkdir -p /project-demo
+
+sudo mkdir -p /project-demo/data
+sudo mkdir -p /project-demo/build
+sudo mkdir -p /project-demo/config
+sudo mkdir -p /project-demo/www
+
+cd /project-demo
