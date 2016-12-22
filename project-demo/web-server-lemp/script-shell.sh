466eef075af7ea6af8b1d80b6fb62e572dacd9c3
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2016-12-22 21:36:15 +0700

    Update script-shell.sh

--- project-demo/web-server-lemp/script-shell.sh
+++ project-demo/web-server-lemp/script-shell.sh
@@ -5,3 +5,5 @@ sudo mkdir -p /project-demo/web-server-lemp/
 sudo mv -R /build/docker-awesome/project-demo/web-server-lemp/ /project-demo/web-server-lemp/
 
 cd /project-demo/web-server-lemp/
+
+sudo docker-compose up -d
