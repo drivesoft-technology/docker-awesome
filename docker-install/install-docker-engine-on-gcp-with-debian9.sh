550f2f4ede765998832fc91195a91dc9fa568eac
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2018-01-03 11:36:02 +0700

    Create install-docker-engine-on-gcp-with-debian9.sh

---
+++ docker-install/install-docker-engine-on-gcp-with-debian9.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get remove docker docker-engine docker.io -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+
+sudo curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-cache policy docker-ce
+
+sudo apt-get install -y docker
+
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
