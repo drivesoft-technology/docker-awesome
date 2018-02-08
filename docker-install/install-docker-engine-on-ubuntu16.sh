4577942cf097890a0816149a6f5ca48ba998c80b
CoachMaxz <eakkabin_it@outlook.co.th>
2018-02-08 15:22:36 +0700

    up phalcon 3.3.0 -> 3.3.1

--- docker-install/install-docker-engine-on-ubuntu16.sh
+++ docker-install/install-docker-engine-on-ubuntu16.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-sudo apt-get install apt-transport-https ca-certificates -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get remove -y docker docker-engine docker.io
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update && sudo apt-cache policy docker-ce
 
 sudo apt-get install -y docker-ce
+sudo apt-cache madison docker-ce
 
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
