24c6ff79bdee6a8ff46f36ea3853f7ef5f01921b
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2018-09-12 18:27:02 +0700

    Update install-docker-compose-on-ubuntu16.sh

--- docker-install/install-docker-compose-on-ubuntu16.sh
+++ docker-install/install-docker-compose-on-ubuntu16.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
