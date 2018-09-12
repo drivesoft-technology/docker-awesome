a6af451caf975fcf92d761c083ab6817e3ecc1b7
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2018-09-12 18:35:04 +0700

    Update install-docker-machine-on-ubuntu16.sh

--- docker-install/install-docker-machine-on-ubuntu16.sh
+++ docker-install/install-docker-machine-on-ubuntu16.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo curl -L https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine
+base=https://github.com/docker/machine/releases/download/v0.14.0
+curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
 sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 
 docker-machine version
