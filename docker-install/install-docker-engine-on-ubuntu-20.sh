2cabbd6bb10ec73038570f537077f97adf0d2d28
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2020-04-24 19:21:35 +0700

    Create install-docker-engine-on-ubuntu-20.sh

---
+++ docker-install/install-docker-engine-on-ubuntu-20.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt install docker.io -y 
+docker --version
+
+sudo systemctl enable --now docker
+sudo usermod -aG docker $(whoami)
+
+sudo docker info
