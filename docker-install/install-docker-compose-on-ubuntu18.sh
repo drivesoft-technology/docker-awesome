0857b575c369c1f3730b3326234ca6d897d95598
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2020-04-24 19:27:33 +0700

    Update install-docker-compose-on-ubuntu18.sh

--- docker-install/install-docker-compose-on-ubuntu18.sh
+++ docker-install/install-docker-compose-on-ubuntu18.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo docker-compose --version
