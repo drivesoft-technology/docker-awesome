ceaba2c6a4e664949709112044f163235dee8b36
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2020-10-20 14:32:43 +0700

    Update install-docker-engine-on-ubuntu-20.sh

--- docker-install/install-docker-engine-on-ubuntu-20.sh
+++ docker-install/install-docker-engine-on-ubuntu-20.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -12,7 +10,10 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 sudo apt-get update
 
-sudo apt install docker.io -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-cache madison docker-ce
+
+sudo apt-get install docker-ce=5:19.03.9~3-0~ubuntu-focal docker-ce-cli=5:19.03.9~3-0~ubuntu-focal containerd.io -y
 docker --version
 
 sudo systemctl enable --now docker
