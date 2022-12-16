e426015ce17b995b59fb4a27d22ea66dea376b24
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2022-12-16 21:51:47 +0700

    Create install-php80-and-composer-on-ubuntu20.sh

---
+++ docker-install/install-php80-and-composer-on-ubuntu20.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# PHP 8.0.26 (FPM)
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt update
+sudo apt install php8.0-fpm -y
+sudo apt autoremove -y
+
+sudo systemctl status php8.0-fpm
+php -v 
+
+# Composer version 1.10
+curl -sS https://getcomposer.org/installer | php
+chmod +x composer.phar
+
+mv composer.phar /usr/local/bin/composer
+composer -V
