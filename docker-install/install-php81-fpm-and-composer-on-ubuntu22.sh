705a60618591e7116a99d93166459d913f16bc4b
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2023-07-20 08:30:03 +0700

    Create install-php81-fpm-and-composer-on-ubuntu22.sh

---
+++ docker-install/install-php81-fpm-and-composer-on-ubuntu22.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install software-properties-common -y
+
+sudo add-apt-repository ppa:ondrej/php -y
+
+# PHP 8.1.21 (cli)
+sudo apt install php8.1 -y
+php -v # PHP 8.1.21 (cli) (built: Jul  8 2023 07:10:19) (NTS)
+
+# Composer version 2.5.8
+curl -sS https://getcomposer.org/installer | php
+chmod +x composer.phar
+
+mv composer.phar /usr/local/bin/composer
+composer -V # Composer version 2.5.8 2023-06-09 17:13:21
