ac8251023b26d217d5709fb3bbe81f66d8f45d9c
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2023-09-14 20:07:14 +0700

    Create install-nodejs-18-on-ubuntu22.sh

---
+++ docker-install/install-nodejs-18-on-ubuntu22.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Node.js 16.19.1
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+sudo apt autoremove -y
+
+node -v
