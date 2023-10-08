b1cfe3d401ccf74f903848d80266e4cf6196432c
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2023-10-08 07:54:43 +0700

    Create install-nginx-on-ubuntu22.sh

---
+++ docker-install/install-nginx-on-ubuntu22.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
+# Nginx
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
+
+# Update
+sudo apt update -y
+sudo apt install nginx -y
+
+# Check
+nginx -v # nginx version: nginx/1.24.0
