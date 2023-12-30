a9fcc68b69d77be1dadf8ad1a6bd628c9a408097
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2023-12-31 03:57:14 +0700

    update

--- docker-install/install-docker-compose-on-ubuntu16.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo docker-compose --version
--- docker-install/install-docker-machine-on-ubuntu16.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-base=https://github.com/docker/machine/releases/download/v0.14.0
-curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
-sudo install /tmp/docker-machine /usr/local/bin/docker-machine
-docker-machine version
