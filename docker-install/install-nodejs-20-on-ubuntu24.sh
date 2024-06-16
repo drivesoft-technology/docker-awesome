bd44da9c0fac6ee9c4c3e560eccf1e65086e039a
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2024-06-16 11:38:49 +0700

    Create install-nodejs-20-on-ubuntu24.sh

---
+++ docker-install/install-nodejs-20-on-ubuntu24.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Node.js 20.14.0
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo apt-get purge nodejs
+sudo apt autoremove -y
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+sudo apt autoremove -y
+
+node -v # v20.14.0
