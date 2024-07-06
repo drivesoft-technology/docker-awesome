99e0976723ddbacfde997665eec0d8cb35749f21
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2024-07-06 11:03:20 +0700

    Update install-nodejs-18-on-ubuntu24.sh

--- docker-install/install-nodejs-18-on-ubuntu24.sh
+++ docker-install/install-nodejs-18-on-ubuntu24.sh
@@ -1,1 +1,20 @@
+#!/bin/sh
 
+# Node.js 18.18.0
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo apt-get purge nodejs
+sudo apt autoremove -y
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+sudo apt autoremove -y
+
+node -v
