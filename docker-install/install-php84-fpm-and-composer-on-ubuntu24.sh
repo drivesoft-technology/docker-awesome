87a4d2d1a4cbf6c6292c27f7d3a3dcea75efacf4
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2025-07-04 04:47:25 +0700

    Update install-php84-fpm-and-composer-on-ubuntu24.sh

--- docker-install/install-php84-fpm-and-composer-on-ubuntu24.sh
+++ docker-install/install-php84-fpm-and-composer-on-ubuntu24.sh
@@ -5,13 +5,13 @@ sudo apt install software-properties-common -y
 
 sudo add-apt-repository ppa:ondrej/php -y
 
-# PHP 8.4.5 (cli)
+# PHP 8.4.10 (cli)
 sudo apt install php8.4 php8.4-fpm php8.4-mysql php8.4-redis php8.4-mongodb php8.4-zip php8.4-gd -y
-php -v # PHP 8.4.5 (cli) (built: Mar 13 2025 15:36:20) (NTS)
+php -v # PHP 8.4.10 (cli) (built: Mar 13 2025 15:36:20) (NTS)
 
-# Composer version 2.8.6
+# Composer version 2.8.9
 curl -sS https://getcomposer.org/installer | php
 chmod +x composer.phar
 
 mv composer.phar /usr/local/bin/composer
-composer -V # Composer version 2.8.6 2025-02-25 13:03:50
+composer -V # Composer version 2.8.9 2025-05-13 14:01:37
